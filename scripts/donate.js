4d9154231cbe8564ee3ef6e92ae7a2c07de6d317
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-11-12 18:37:16 +0530

    almost done

--- scripts/donate.js
+++ scripts/donate.js
@@ -39,6 +39,68 @@ const Dona = () => {
 };
 
 
+const Foot = () => {
+    return (
+        <div>
+            <footer class="footer">
+                <div class="wrapper">
+
+
+
+
+
+                    <div class="footer_middle">
+                        <div class="footer_middle_left">
+                            <h5>Follow Us</h5>
+                            <div class="footer_middle_left_list">
+                                <div class="footer_icons">
+                                    <a href="https://www.facebook.com/"><img src="../assets/facebook.svg" alt="facebook" /></a>
+                                </div>
+                                <div class="footer_icons">
+                                    <a href="https://www.linkedin.com">
+                                        <img src="../assets/linkedIn.svg" alt="linkedIn" /></a>
+                                </div>
+                                <div class="footer_icons">
+                                    <a href="https://twitter.com/?lang=en">
+                                        <img src="../assets/twitter.svg" alt="twitter" /></a>
+                                </div>
+                                <div class="footer_icons">
+                                    <a href="https://www.youtube.com/">
+                                        <img src="../assets/youtube.svg" alt="youtube" /></a>
+                                </div>
+                                <div class="footer_icons">
+                                    <a href="https://www.instagram.com/">
+                                        <img src="../assets/instagram.svg" alt="instagram" /></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="footer_middle_right">
+                            <h5>Mobile app</h5>
+                            <div class="footer_middle_right_list">
+                                <div class="footer_icons">
+                                    <img src="../assets/apple.svg" alt="apple" />
+                                </div>
+                                <div class="footer_icons">
+                                    <img src="../assets/android.svg" alt="android" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer_bottom">
+                        <p>© 2023 Educatalyst®</p>
+                        <ul class="footer_bottom_list">
+                            <li> <a href="../aboutus.html">  About Us</a></li>
+                            <li><a href="../contact.html">Contact Us</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
 
 
 ReactDOM.render(<Dona />, document.getElementById('donat'));
+ReactDOM.render(<Foot />, document.getElementById('fot'));
