05e33389c195210abed04a46a86a55aa5924ef3e
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-11-12 13:47:59 +0530

    commit

--- scripts/involve.js
+++ scripts/involve.js
@@ -145,16 +145,82 @@ const Involv = () => {
 
             <div className="container">
                 <div>
-                    <h1 id="h1">Volunteering wihtEducatalyst</h1>
-                    <h5>Become a driving force for positive change by volunteering with Educatalyst. Your time and skills can make a significant impact on the lives of underprivileged children. As a volunteer, you have the opportunity to directly contribute to the educational journey of these young learners. Whether you're passionate about teaching, mentoring, or organizing events, there's a place for you at Educatalyst. Join our team of dedicated individuals committed to building a brighter future through education.</h5>
+                    <h1 id="h1">Volunteering with Educatalyst</h1>
+                    <h5 id="h5">Become a driving force for positive change by volunteering with Educatalyst. Your time and skills can make a significant impact on the lives of underprivileged children. As a volunteer, you have the opportunity to directly contribute to the educational journey of these young learners. Whether you're passionate about teaching, mentoring, or organizing events, there's a place for you at Educatalyst. Join our team of dedicated individuals committed to building a brighter future through education.</h5>
+                    <h3 id="h3">Click the button below to volunteer</h3>
                 </div>
                 <div className="col-lg-12 text-center">
                     <button id="btn1"><a href="https://gokulnandakumar04.wixsite.com/volunteer">Volunteer</a></button>
+                </div>
+                <div>
+                    <h1 id="h1">Donating to Educatalyst</h1>
+                    <h5 id="h5">Your support can be the key to unlocking a world of opportunities for underprivileged children. By making a donation to Educatalyst, you directly contribute to providing quality education, resources, and a brighter future for those who need it the most.
+
+                    </h5>
+                    <h3 id="h3">Click the button below to Donate</h3>
                 </div>
                 <div className="col-lg-12 text-center">
                     <button id="btn1"><a href="../donate.html">Donation Form</a></button>
                 </div>
             </div>
+
+
+
+
+            <footer class="footer">
+                <div class="wrapper">
+
+
+
+
+
+                    <div class="footer_middle">
+                        <div class="footer_middle_left">
+                            <h5>Follow Us</h5>
+                            <div class="footer_middle_left_list">
+                                <div class="footer_icons">
+                                    <a href="https://www.facebook.com/"><img src="../assets/facebook.svg" alt="facebook" /></a>
+                                </div>
+                                <div class="footer_icons">
+                                    <a href="https://www.linkedin.com">
+                                        <img src="../assets/linkedIn.svg" alt="linkedIn" /></a>
+                                </div>
+                                <div class="footer_icons">
+                                    <a href="https://twitter.com/?lang=en">
+                                        <img src="../assets/twitter.svg" alt="twitter" /></a>
+                                </div>
+                                <div class="footer_icons">
+                                    <a href="https://www.youtube.com/">
+                                        <img src="../assets/youtube.svg" alt="youtube" /></a>
+                                </div>
+                                <div class="footer_icons">
+                                    <a href="https://www.instagram.com/">
+                                        <img src="../assets/instagram.svg" alt="instagram" /></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="footer_middle_right">
+                            <h5>Mobile app</h5>
+                            <div class="footer_middle_right_list">
+                                <div class="footer_icons">
+                                    <img src="../assets/apple.svg" alt="apple" />
+                                </div>
+                                <div class="footer_icons">
+                                    <img src="../assets/android.svg" alt="android" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer_bottom">
+                        <p>© 2023 Educatalyst®</p>
+                        <ul class="footer_bottom_list">
+                            <li> <a href="../aboutus.html">  About Us</a></li>
+                            <li><a href="../contact.html">Contact Us</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </footer>
 
 
 
